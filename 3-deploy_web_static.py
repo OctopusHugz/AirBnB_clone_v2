15a1abed1569f1c736e2c53d22890df54e657433
OctopusHugz <colsonscott53@gmail.com>
2020-08-17 23:21:52 +0000

    Added outline for exercise 3

---
+++ 3-deploy_web_static.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+from fabric.state import env
+
+env.hosts = ['35.231.246.19', '34.227.48.93']
+pack_module = __import__("1-pack_web_static")
+deploy_module = __import__("2-do_deploy_web_static")
+
+
+def deploy():
+    """This function packs a tarball and deploys it to multiple web servers"""
+    archive_path = pack_module.do_pack()
+    if archive_path == None:
+        return False
+    deploy_status = deploy_module.do_deploy(archive_path)
+    return deploy_status
