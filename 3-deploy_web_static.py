6f3e59104695f0c42d9cfa3a546662df1adc9ac0
OctopusHugz <colsonscott53@gmail.com>
2020-08-17 23:23:30 +0000

    Fixed pep8 error

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -9,7 +9,7 @@ deploy_module = __import__("2-do_deploy_web_static")
 def deploy():
     """This function packs a tarball and deploys it to multiple web servers"""
     archive_path = pack_module.do_pack()
-    if archive_path == None:
+    if archive_path is None:
         return False
     deploy_status = deploy_module.do_deploy(archive_path)
     return deploy_status
