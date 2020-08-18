de0a1235729d8ecf9269f68874cf48b2b95c2ec6
OctopusHugz <colsonscott53@gmail.com>
2020-08-18 03:05:47 +0000

    Added second removal to script from /data/web_static/releases

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -3,14 +3,16 @@ from fabric.operations import run
 from fabric.state import env
 
 env.hosts = ['35.231.246.19', '34.227.48.93']
-pack_module = __import__("1-pack_web_static")
-deploy_module = __import__("2-do_deploy_web_static")
 
 
 def do_clean(number=0):
     """This function deletes out-of-date archives"""
     if number in [0, 1]:
         run("ls -1t versions | awk 'NR > 1' | xargs rm")
+        run("ls -1t /data/web_static/releases | awk 'NR > 1' | xargs rm")
     else:
-        cmd_string = "ls -1t versions | awk 'NR > " + number + "' | xargs rm"
-        run(cmd_string)
+        v_cmd_string = "ls -1t versions | awk 'NR > " + number + "' | xargs rm"
+        run(v_cmd_string)
+        r_cmd_string = "ls -1t /data/web_static/releases | awk 'NR > "\
+            + number + "' | xargs rm"
+        run(r_cmd_string)
