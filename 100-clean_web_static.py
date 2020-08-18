48c240a515bcf40a73b2dc071637bbf2c639c0b3
OctopusHugz <colsonscott53@gmail.com>
2020-08-18 03:10:06 +0000

    Added local/run mix

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-from fabric.operations import run
+from fabric.operations import local, run
 from fabric.state import env
 
 env.hosts = ['35.231.246.19', '34.227.48.93']
@@ -8,11 +8,11 @@ env.hosts = ['35.231.246.19', '34.227.48.93']
 def do_clean(number=0):
     """This function deletes out-of-date archives"""
     if number in [0, 1]:
-        run("ls -1t versions | awk 'NR > 1' | xargs rm")
+        local("ls -1t versions | awk 'NR > 1' | xargs rm")
         run("ls -1t /data/web_static/releases | awk 'NR > 1' | xargs rm")
     else:
         v_cmd_string = "ls -1t versions | awk 'NR > " + number + "' | xargs rm"
-        run(v_cmd_string)
+        local(v_cmd_string)
         r_cmd_string = "ls -1t /data/web_static/releases | awk 'NR > "\
             + number + "' | xargs rm"
         run(r_cmd_string)
