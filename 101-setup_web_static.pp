77f5c40882a60f66c5d484c32e661bfe319f3b7c
OctopusHugz <colsonscott53@gmail.com>
2020-08-18 04:35:14 +0000

    Added /usr/bin in front of all commands. Removed

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,10 +1,9 @@
 # This manifest configures a server specifically
 $update = '/usr/bin/env apt-get -y update'
-$string='\\t\\talias /data/web_static/current/;'
-$command1 = "/usr/bin/env sed -i '37a \\tlocation /hbnb_static/ {' /etc/nginx/sites-available/default"
+$string='alias /data/web_static/current/;'
+$command1 = "/usr/bin/env sed -i '37a location /hbnb_static/ {' /etc/nginx/sites-available/default"
 $command2 = "/usr/bin/env sed -i '38a ${string}' /etc/nginx/sites-available/default"
-$command3 = "/usr/bin/env sed -i '39a \\t}' /etc/nginx/sites-available/default"
-$command4 = "/usr/bin/env sed -i '40a \\n' /etc/nginx/sites-available/default"
+$command3 = "/usr/bin/env sed -i '39a }' /etc/nginx/sites-available/default"
 
 exec { 'apt-get update':
   command => $update
@@ -16,15 +15,15 @@ exec { 'apt-get update':
 }
 
 -> exec { 'mkdir1':
-  command => '/bin mkdir -p /data/web_static/releases/test'
+  command => '/usr/bin/env mkdir -p /data/web_static/releases/test'
 }
 
 -> exec { 'mkdir2':
-  command => '/bin mkdir -p /data/web_static/shared'
+  command => '/usr/bin/env mkdir -p /data/web_static/shared'
 }
 
 -> exec { 'index.html content':
-  command => '/bin echo "<html>
+  command => '/usr/bin/env echo "<html>
   <head>
   </head>
   <body>
@@ -34,11 +33,11 @@ exec { 'apt-get update':
 }
 
 -> exec { 'symlink':
-  command => '/bin ln -sfn /data/web_static/releases/test/ /data/web_static/current'
+  command => '/usr/bin/env ln -sfn /data/web_static/releases/test/ /data/web_static/current'
 }
 
 -> exec { 'chown':
-  command => '/bin chown -R ubuntu:ubuntu /data'
+  command => '/usr/bin/env chown -R ubuntu:ubuntu /data'
 }
 
 -> exec { 'sed1':
@@ -51,10 +50,6 @@ exec { 'apt-get update':
 
 -> exec { 'sed3':
   command => $command3
-}
-
--> exec { 'sed4':
-  command => $command4
 }
 
 -> service { 'nginx':
