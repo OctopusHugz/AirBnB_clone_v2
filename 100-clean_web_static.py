b1a4a8b66f6cc9bbf12eb62eebceb392c3c94dac
OctopusHugz <colsonscott53@gmail.com>
2020-08-18 04:30:09 +0000

    Changed ls to ltr

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -8,13 +8,13 @@ env.hosts = ['35.231.246.19', '34.227.48.93']
 def do_clean(number=0):
     """This function deletes out-of-date archives"""
     if number in [0, 1]:
-        local("ls -1t versions | awk 'NR > 1' | xargs rm -rf")
-        run("""ls -1t /data/web_static/releases | awk 'NR > 1'
+        local("ls -ltr versions | awk 'NR > 1' | xargs rm -rf")
+        run("""ls -ltr /data/web_static/releases | awk 'NR > 1'
          | grep -v 'test' | xargs rm -rf""")
     else:
-        v_cmd_string = "ls -1t versions | awk 'NR > "\
+        v_cmd_string = "ls -ltr versions | awk 'NR > "\
             + number + "' | xargs rm -rf"
         local(v_cmd_string)
-        r_cmd_string = "ls -1t /data/web_static/releases | awk 'NR > "\
+        r_cmd_string = "ls -ltr /data/web_static/releases | awk 'NR > "\
             + number + "' | grep -v 'test' | xargs rm -rf"
         run(r_cmd_string)
