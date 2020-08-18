62da646c06d196c18a33e8110a327b68198ecbf0
OctopusHugz <colsonscott53@gmail.com>
2020-08-18 03:16:49 +0000

    Added -f to rm command:

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -8,11 +8,12 @@ env.hosts = ['35.231.246.19', '34.227.48.93']
 def do_clean(number=0):
     """This function deletes out-of-date archives"""
     if number in [0, 1]:
-        local("ls -1t versions | awk 'NR > 1' | xargs rm")
-        run("ls -1t /data/web_static/releases | awk 'NR > 1' | xargs rm")
+        local("ls -1t versions | awk 'NR > 1' | xargs rm -f")
+        run("ls -1t /data/web_static/releases | awk 'NR > 1' | xargs rm -f")
     else:
-        v_cmd_string = "ls -1t versions | awk 'NR > " + number + "' | xargs rm"
+        v_cmd_string = "ls -1t versions | awk 'NR > "\
+            + number + "' | xargs rm -f"
         local(v_cmd_string)
         r_cmd_string = "ls -1t /data/web_static/releases | awk 'NR > "\
-            + number + "' | xargs rm"
+            + number + "' | xargs rm -f"
         run(r_cmd_string)
