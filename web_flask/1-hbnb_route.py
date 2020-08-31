097ba052119fdc3f72bda283a4afc1b1fd1725d2
OctopusHugz <colsonscott53@gmail.com>
2020-08-31 19:18:48 +0000

    Added solution for exercise 1

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""This module starts a Flask web application"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """This function returns a string when accessing the root index"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """This function returns a string when accessing the /hbnb route"""
+    return "HBNB"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
