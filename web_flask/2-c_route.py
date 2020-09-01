d0bbbd8c1ae2165539fc2f79d790801a4c36fc02
OctopusHugz <colsonscott53@gmail.com>
2020-09-01 02:44:24 +0000

    Added solution for exercise 2

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""This module starts a Flask web application"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """This function returns a string when accessing the root index"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """This function returns a string when accessing the /hbnb route"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    """This function returns a string when accessing the /c/<text> route"""
+    return "C " + text.replace("_", " ")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
