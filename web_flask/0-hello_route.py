37056adaa428eb02ddab4cc6729441d022e6be1a
OctopusHugz <colsonscott53@gmail.com>
2020-09-01 21:44:18 +0000

    Removed host and went back to per file app instance

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """This module starts a Flask web application"""
-from web_flask import app
-
+from flask import Flask
+app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def index():
@@ -10,4 +10,4 @@ def index():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0")
