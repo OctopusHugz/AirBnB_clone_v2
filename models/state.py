a6792e8e720862c1ecf06f4e848569135a030aa0
OctopusHugz <colsonscott53@gmail.com>
2020-09-02 03:40:48 +0000

    Added list creating logic for cities getter in state.py

--- models/state.py
+++ models/state.py
@@ -4,6 +4,7 @@ from os import getenv
 from sqlalchemy.orm import relationship
 from models.base_model import Base, BaseModel, Column, String
 from models.city import City
+from models import storage
 
 
 class State(BaseModel, Base):
@@ -14,8 +15,13 @@ class State(BaseModel, Base):
                           backref="state", passive_deletes=True)
 
 
-if getenv('HBNB_TYPE_STORAGE') != 'db':
-    @property
-    def cities(self):
-        """ Cities getter """
-        return self.cities
+    if getenv('HBNB_TYPE_STORAGE') != 'db':
+        @property
+        def cities(self):
+            """ Cities getter """
+            cities_list = []
+            cities_dict = storage.all(City)
+            for city in cities_dict.values():
+                if self.id == city.state_id:
+                    cities_list.append(city)
+            return cities_list
