b0f8c237276eccec345b7289fc3e31ca0b6f94b0
OctopusHugz <colsonscott53@gmail.com>
2020-09-02 03:14:03 +0000

    Try adding if type storage != db for cities getter. May need to tweak logic

--- models/state.py
+++ models/state.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+from os import getenv
 from sqlalchemy.orm import relationship
 from models.base_model import Base, BaseModel, Column, String
 from models.city import City
@@ -12,6 +13,8 @@ class State(BaseModel, Base):
     cities = relationship("City", cascade="all, delete-orphan",
                           backref="state", passive_deletes=True)
 
+
+if getenv('HBNB_TYPE_STORAGE') != 'db':
     @property
     def cities(self):
         """ Cities getter """
