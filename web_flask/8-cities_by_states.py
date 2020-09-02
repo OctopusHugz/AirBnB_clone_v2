8fed1ee9caa1301461079cdaa74c0be97078ae89
OctopusHugz <colsonscott53@gmail.com>
2020-09-02 05:29:18 +0000

    Switch if logic to == db from != db

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -13,10 +13,10 @@ def cities_by_states():
     """This function returns HTML when accessing the /cities_by_states route"""
     states = storage.all(State).values()
     for state in states:
-        if getenv('HBNB_TYPE_STORAGE') != 'db':
-            cities = storage.all(City).values()
-        else:
+        if getenv('HBNB_TYPE_STORAGE') == 'db':
             cities = state.cities
+        else:
+            cities = storage.all(City).values()
     return render_template('8-cities_by_states.html', states=states)
 
 
