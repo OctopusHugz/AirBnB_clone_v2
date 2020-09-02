de6d60474c7f93332611e5fa1cf29448bb9692f0
OctopusHugz <colsonscott53@gmail.com>
2020-09-02 17:17:41 +0000

    Added outline for all files for exercise 11. Need to add jinja templating and import objects from storage still

---
+++ web_flask/10-hbnb_filters.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""This module starts a Flask web application"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+app = Flask(__name__)
+
+
+@app.route('/hbnb_filters', strict_slashes=False)
+def hbnb_filters():
+    """This function returns HTML when accessing the /states route"""
+    states = storage.all(State).values()
+    return render_template('10-hbnb_filters.html', states=states)
+
+
+@app.teardown_appcontext
+def closing_time(self):
+    """This function tears down and closes session"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
